97ac5805210b5b0c0108ee2d642f0c322c39e364
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-07-03 23:47:58 +0530

    blob object type added

---
+++ got/blob.go
@@ -0,0 +1,20 @@
+package got
+
+type blob struct {
+	blobdata []byte
+	repo     Repo
+}
+
+func (b blob) Serialize() []byte {
+	return b.blobdata
+}
+func (b blob) Deserialize(data []byte) {
+	b.blobdata = data
+}
+
+func (b blob) get_type() []byte {
+	return []byte("blob")
+}
+func (b blob) get_repo() Repo {
+	return b.repo
+}
